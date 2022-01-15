e5c26b0564e9f890005d1d2fe638a43e4856b044
ArtemBrest <artoym.zan@gmail.com>
2022-01-15 13:49:47 +0300

    lab

--- OAIP/OAIP_lab2/OAIP_lab2/Program.cs
+++ OAIP/OAIP_lab2/OAIP_lab2/Program.cs
@@ -6,35 +6,41 @@ namespace OAIP_lab2
     {
         static void Main(string[] args)
         {
-			
-			Console.Write("x1 = ");
-			double x1 = double.Parse(Console.ReadLine());
-			Console.Write("y1 = ");
-			double y1 = double.Parse(Console.ReadLine());
-			Console.Write("r1 = ");
-			double r1 = double.Parse(Console.ReadLine());
-			Console.Write("x2 = ");
-			double x2 = double.Parse(Console.ReadLine());
-			Console.Write("y2 = ");
-			double y2 = double.Parse(Console.ReadLine());
-			Console.Write("r2 = ");
-			double r2 = double.Parse(Console.ReadLine());
-
+			double x1 = 0, y1 = 0, r1 = 0, x2 = 0, y2 = 0, r2 = 0;
+			try
+			{
+				Console.Write("x1 = ");
+				x1 = Convert.ToDouble(Console.ReadLine());
+				Console.Write("y1 = ");
+				y1 = Convert.ToDouble(Console.ReadLine());
+				Console.Write("r1 = ");
+				r1 = Convert.ToDouble(Console.ReadLine());
+				Console.Write("x2 = ");
+				x2 = Convert.ToDouble(Console.ReadLine());
+				Console.Write("y2 = ");
+				y2 = Convert.ToDouble(Console.ReadLine());
+				Console.Write("r2 = ");
+				r2 = Convert.ToDouble(Console.ReadLine());
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine("Входная строка не имела числового формата.");
+			}
 			if (x1 == x2 && y1 == y2 && r1 == r2)
 			{
-				Console.Write("Окружности совпадают");
+				Console.WriteLine("Окружности совпадают");
 			}
 			else if (Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) == r1 + r2 || Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r2 == r1 || Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r1 == r2)
 			{
-				Console.Write("Пересекаются в 1 точке"); // (1,1,4) (9,1,4)
+				Console.WriteLine("Пересекаются в 1 точке"); // (1,1,4) (9,1,4)
 			}
 			else if (Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) > r1 + r2)
 			{
-				Console.Write("Не пересекаются!");
+				Console.WriteLine("Не пересекаются!");
 			}
 			else if (Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r2 < r1 || Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)) + r1 < r2)
 			{
-				Console.Write("Не пересекаются!");
+				Console.WriteLine("Не пересекаются!");
 			}
 			else
 			{
--- OAIP/OAIP_lab3/OAIP_lab3/Program.cs
+++ OAIP/OAIP_lab3/OAIP_lab3/Program.cs
@@ -6,29 +6,28 @@ namespace OAIP_lab3
     {
         static void Main(string[] args)
         {
-            const int N = 2;
+            const int N = 2;          
+            int a, result = 0;
             Console.WriteLine("Введите число в 2 системе счисления: ");
             string A = Console.ReadLine();
-            int a;
             try
             {
                 a = Convert.ToInt32(A);
-                int result = 0;
-                for (int i = A.Length - 1, mul = 0; i >= 0; i--, mul++) {
+                for (int i = A.Length - 1, mul = 0; i >= 0; i--, mul++)
+                {
                     //result += (int)Math.Pow(N, mul) * (Char.IsDigit(A[i]) ? (int)Char.GetNumericValue(A[i]) : (A[i] - 55));                   
-                    if (Char.IsDigit(A[i])){
+                    if (Char.IsDigit(A[i]))
+                    {
                         result += (int)Math.Pow(N, mul) * (int)Char.GetNumericValue(A[i]);
                     }
                     else
                     {
                         result += (int)Math.Pow(N, mul) * (A[i] - 55);
                     }
-                    Console.WriteLine(result);
-                    Console.WriteLine("_____");
                 }
                 Console.WriteLine("Число в 10 системе счисления: " + result);
             }
-            catch
+            catch (FormatException e)
             {
                 Console.WriteLine("Введено не int");
             }
