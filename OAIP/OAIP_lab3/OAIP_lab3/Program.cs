a4f2bdd1b64ded87e419fd54ba53072a1b18d9dd
ArtemBrest <artoym.zan@gmail.com>
2022-01-15 13:12:05 +0300

    lab

--- OAIP/OAIP_lab3/OAIP_lab3/Program.cs
+++ OAIP/OAIP_lab3/OAIP_lab3/Program.cs
@@ -9,11 +9,29 @@ namespace OAIP_lab3
             const int N = 2;
             Console.WriteLine("Введите число в 2 системе счисления: ");
             string A = Console.ReadLine();
-            
-            int result = 0;
-            for (int i = A.Length - 1, mul = 0; i >= 0; i--, mul++)
-                result += (int)Math.Pow(N, mul) * (Char.IsDigit(A[i]) ? (int)Char.GetNumericValue(A[i]) : (A[i] - 55));
-            Console.WriteLine("Число в 10 системе счисления: " + result);
+            int a;
+            try
+            {
+                a = Convert.ToInt32(A);
+                int result = 0;
+                for (int i = A.Length - 1, mul = 0; i >= 0; i--, mul++) {
+                    //result += (int)Math.Pow(N, mul) * (Char.IsDigit(A[i]) ? (int)Char.GetNumericValue(A[i]) : (A[i] - 55));                   
+                    if (Char.IsDigit(A[i])){
+                        result += (int)Math.Pow(N, mul) * (int)Char.GetNumericValue(A[i]);
+                    }
+                    else
+                    {
+                        result += (int)Math.Pow(N, mul) * (A[i] - 55);
+                    }
+                    Console.WriteLine(result);
+                    Console.WriteLine("_____");
+                }
+                Console.WriteLine("Число в 10 системе счисления: " + result);
+            }
+            catch
+            {
+                Console.WriteLine("Введено не int");
+            }
             Console.ReadKey();
         }
     }
